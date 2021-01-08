ed5ccfc5d96799ebc6da3e866a1ef55633ac3045
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-01-08 15:07:14 +0000

    Copy env vars and secrets from old publisher task def
    TODO: update the references to variables that don't exist...

--- terraform/deployments/apps/publisher/main.tf
+++ terraform/deployments/apps/publisher/main.tf
@@ -87,37 +87,66 @@ locals {
   sentry_environment = "${var.environment}-ecs"
   statsd_host        = "statsd.${local.mesh_domain}" # TODO: Put Statsd in App Mesh
 
+  # TODO fix all the var. whatevers.
   environment_variables = {
-    DEFAULT_TTL                     = 1800,
-    GOVUK_APP_DOMAIN                = local.mesh_domain,
-    GOVUK_APP_DOMAIN_EXTERNAL       = local.app_domain,
-    GOVUK_APP_NAME                  = "publisher",
-    GOVUK_APP_TYPE                  = "rack",
-    GOVUK_CONTENT_SCHEMAS_PATH      = "/govuk-content-schemas",
-    GOVUK_GROUP                     = "deploy",  # TODO: clean up?
-    GOVUK_STATSD_PREFIX             = "fargate", # TODO: use a better prefix?
-    GOVUK_USER                      = "deploy",  # TODO: clean up?
-    GOVUK_WEBSITE_ROOT              = local.govuk_website_root,
-    PLEK_SERVICE_PUBLISHING_API_URI = "http://publishing-api-web.${local.mesh_domain}",
-    PLEK_SERVICE_ROUTER_API_URI     = "http://router-api.${local.mesh_domain}",
-    PLEK_SERVICE_RUMMAGER_URI       = "",
-    PLEK_SERVICE_SIGNON_URI         = "https://signon-ecs.${local.app_domain}",
-    PLEK_SERVICE_SPOTLIGHT_URI      = "",
-    PORT                            = 80,
-    RAILS_ENV                       = "production",
-    SENTRY_ENVIRONMENT              = local.sentry_environment,
-    STATSD_PROTOCOL                 = "tcp",
-    STATSD_HOST                     = local.statsd_host,
-    UNICORN_WORKER_PROCESSES        = 12,
+    ASSET_HOST                       = var.asset_host,
+    BASIC_AUTH_USERNAME              = "gds",
+    EMAIL_GROUP_BUSINESS             = "test-address@digital.cabinet-office.gov.uk",
+    EMAIL_GROUP_CITIZEN              = "test-address@digital.cabinet-office.gov.uk",
+    EMAIL_GROUP_DEV                  = "test-address@digital.cabinet-office.gov.uk",
+    EMAIL_GROUP_FORCE_PUBLISH_ALERTS = "test-address@digital.cabinet-office.gov.uk",
+    FACT_CHECK_SUBJECT_PREFIX        = "dev",
+    FACT_CHECK_USERNAME              = "govuk-fact-check-test@digital.cabinet-office.gov.uk",
+    GOVUK_APP_DOMAIN                 = var.service_discovery_namespace_name,
+    GOVUK_APP_DOMAIN_EXTERNAL        = var.govuk_app_domain_external,
+    GOVUK_APP_NAME                   = "publisher",
+    GOVUK_APP_ROOT                   = "/app",
+    GOVUK_APP_TYPE                   = "rack",
+    GOVUK_STATSD_PREFIX              = "fargate",
 
-    PLEK_SERVICE_PERFORMANCEPLATFORM_BIG_SCREEN_VIEW_URI = "",
+    # TODO: how does GOVUK_ASSET_ROOT relate to ASSET_HOST? Is one a function of the other? Are they both really in use? Is GOVUK_ASSET_ROOT always just https://${ASSET_HOST}?
+    GOVUK_ASSET_ROOT                = "https://assets.test.publishing.service.gov.uk",
+    GOVUK_GROUP                     = "deploy",
+    GOVUK_USER                      = "deploy",
+    GOVUK_WEBSITE_ROOT              = var.govuk_website_root,
+    PLEK_SERVICE_CONTENT_STORE_URI  = "https://www.gov.uk/api", # TODO: looks suspicious
+    PLEK_SERVICE_PUBLISHING_API_URI = "http://publishing-api-web.${var.service_discovery_namespace_name}",
+    PLEK_SERVICE_SIGNON_URI         = "https://signon-ecs.${var.govuk_app_domain_external}",
+    PLEK_SERVICE_STATIC_URI         = "https://assets.test.publishing.service.gov.uk",
+
+    # TODO: remove PLEK_SERVICE_DRAFT_ORIGIN_URI once we have the draft origin properly set up with multiple frontends
+    PLEK_SERVICE_DRAFT_ORIGIN_URI = "https://draft-frontend.${var.govuk_app_domain_external}",
+    PORT                          = "80",
+    RAILS_ENV                     = "production",
+    RAILS_SERVE_STATIC_FILES      = "true", # TODO: temporary hack?
+
+    # TODO: we shouldn't be specifying both REDIS_{HOST,PORT} *and* REDIS_URL.
+    REDIS_HOST         = var.redis_host,
+    REDIS_PORT         = tostring(var.redis_port),
+    REDIS_URL          = "redis://${var.redis_host}:${var.redis_port}",
+    STATSD_PROTOCOL    = "tcp",
+    STATSD_HOST        = var.statsd_host,
+    WEBSITE_ROOT       = var.govuk_website_root,
+    SENTRY_ENVIRONMENT = var.sentry_environment
   }
 
   secrets_from_arns = {
+    ASSET_MANAGER_BEARER_TOKEN    = data.aws_secretsmanager_secret.asset_manager_bearer_token.arn,
+    FACT_CHECK_PASSWORD           = data.aws_secretsmanager_secret.fact_check_password.arn,
+    FACT_CHECK_REPLY_TO_ADDRESS   = data.aws_secretsmanager_secret.fact_check_reply_to_address.arn,
+    FACT_CHECK_REPLY_TO_ID        = data.aws_secretsmanager_secret.fact_check_reply_to_id.arn,
+    GOVUK_NOTIFY_API_KEY          = data.aws_secretsmanager_secret.govuk_notify_api_key.arn,
+    GOVUK_NOTIFY_TEMPLATE_ID      = data.aws_secretsmanager_secret.govuk_notify_template_id.arn,
+    JWT_AUTH_SECRET               = data.aws_secretsmanager_secret.jwt_auth_secret.arn,
+    LINK_CHECKER_API_BEARER_TOKEN = data.aws_secretsmanager_secret.link_checker_api_bearer_token.arn,
+    LINK_CHECKER_API_SECRET_TOKEN = data.aws_secretsmanager_secret.link_checker_api_secret_token.arn,
+
+    # TODO: Only the password should be a secret in the MONGODB_URI.
+    MONGODB_URI                 = data.aws_secretsmanager_secret.mongodb_uri.arn,
     GDS_SSO_OAUTH_ID            = data.aws_secretsmanager_secret.oauth_id.arn,
     GDS_SSO_OAUTH_SECRET        = data.aws_secretsmanager_secret.oauth_secret.arn,
     PUBLISHING_API_BEARER_TOKEN = data.aws_secretsmanager_secret.publishing_api_bearer_token.arn,
     SECRET_KEY_BASE             = data.aws_secretsmanager_secret.secret_key_base.arn,
-    SENTRY_DSN                  = data.aws_secretsmanager_secret.sentry_dsn.arn,
+    SENTRY_DSN                  = data.aws_secretsmanager_secret.sentry_dsn.arn
   }
 }
